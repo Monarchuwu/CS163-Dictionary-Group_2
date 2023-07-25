a23ee9ec1ca8fdea36c54eb7c5b385fb3c8b2bd2
PkKeo <minhcvn2011@gmail.com>
2023-07-25 23:00:45 +0700

    Finish favourite screen + Trie function

--- src/favouriteScreen/fileWork.h
+++ src/favouriteScreen/fileWork.h
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include "MyTrie.h"
+
 namespace minh
 {
     std::string defOfWord(std::string word, const std::string& filename);
 
-    void addToEndOfFile(std::string word, const std::string& filename, const std::string &rootfilename);
+    void addToEndOfFile(int id, const std::string& filename, std::vector<Words::Word> v);
 
     std::string takeLine(int n, const std::string& filename);
     
