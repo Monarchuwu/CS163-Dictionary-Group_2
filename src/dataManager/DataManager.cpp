4812c89023fd839721335e40b775cfeaaabec3a6
Monarchuwu <75823146+Monarchuwu@users.noreply.github.com>
2023-07-27 08:05:20 +0700

    feat/create-a-basic-DataManager-class there are 2 feats: - set/get dataset and set/get modeSearch (information for loading/saving feats) - load/save/reset data from dataset functions (load/save dataset internal are auxiliary functions to handle .txt)

---
+++ src/dataManager/DataManager.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "DataManager.h"
+
+DataManager::DataManager() {
+    mDataset = mModeSearch = 0;
+}
+DataManager::~DataManager() {}
+
+void DataManager::setDataset(int dataset) {
+    mDataset = dataset;
+}
+int DataManager::getDataset() {
+    return mDataset;
+}
+void DataManager::setModeSearch(int modeSearch) {
+    mModeSearch = modeSearch;
+}
+int DataManager::getModeSearch() {
+    return mModeSearch;
+}
+
+void DataManager::loadData() {
+    switch (mDataset) {
+        case Dataset::Slang:
+            loadDatasetInternal("data/slang/data.txt");
+            break;
+
+        default:
+            std::cout << "[ERROR] The dataset is not available to load" << std::endl;
+            break;
+    }
+}
+void DataManager::saveData() {
+    switch (mDataset) {
+        case Dataset::Slang:
+            saveDatasetInternal("data/slang/data.txt");
+            break;
+
+        default:
+            std::cout << "[ERROR] The dataset is not available to save" << std::endl;
+            break;
+    }
+}
+void DataManager::resetData() {
+    switch (mDataset) {
+        case Dataset::Slang:
+            loadDatasetInternal("data/slang/backup.txt");
+            break;
+
+        default:
+            std::cout << "[ERROR] The dataset is not available to reset" << std::endl;
+            break;
+    }
+}
+
+void DataManager::loadDatasetInternal(const std::string& dirDataset) {
+}
+
+void DataManager::saveDatasetInternal(const std::string& dirDataset) {
+}
---
+++ src/dataManager/DataManager.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <string>
+
+class DataManager {
+public:
+    static enum Dataset {
+        EngEng,
+        EngVie,
+        VieEng,
+        Slang,
+        Emoji
+    };
+
+    static enum ModeSearch {
+        SearchByWord,
+        SearchByDefinition
+    };
+
+public:
+    // constructor
+    DataManager();
+
+    // destructor
+    ~DataManager();
+
+    /* ----- update dataset and mode search information ----- */
+
+    // set the dataset to load
+    void setDataset(int dataset);
+    // get the dataset
+    int getDataset();
+    // set the mode search
+    void setModeSearch(int mode);
+    // get the mode search
+    int getModeSearch();
+
+    /* ----- handle dataset ----- */
+
+    // load data from file
+    void loadData();
+    // save data to file
+    void saveData();
+    // reset data to default
+    /// this function will load data from backup file
+    void resetData();
+
+    /* ----- handle search word query ----- */
+
+    /* ----- handle search definition query ----- */
+
+private:
+    // load data from file (internal)
+    void loadDatasetInternal(const std::string& dirDataset);
+    // save data to file (internal)
+    void saveDatasetInternal(const std::string& dirDataset);
+
+private:
+    // dataset
+    int mDataset;
+    // mode search
+    int mModeSearch;
+
+    // store the data of dictionary
+};
