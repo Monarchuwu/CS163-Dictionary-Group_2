d70d3d2d5bf40599da0bdac64d98a629ef1bf329
Monarchuwu <75823146+Monarchuwu@users.noreply.github.com>
2023-07-27 16:47:05 +0700

    feat/add-OpenSans-font the font is added into data/fonts/ we can use it by including Contant.h in src/ and using constant::fontOpenSans variable

--- src/Constant.cpp
+++ src/Constant.cpp
@@ -3,9 +3,14 @@
 
 namespace constant {
     sf::Font fontArial;
+    sf::Font fontOpenSans;
 
 	void loadFont() {
 		if (!fontArial.loadFromFile("data/fonts/arial.ttf")) {
+			std::cout << "Can't load the font !!!";
+			exit(0);
+		}
+		if (!fontOpenSans.loadFromFile("data/fonts/Open_Sans/OpenSans.ttf")) {
 			std::cout << "Can't load the font !!!";
 			exit(0);
 		}
--- src/Constant.h
+++ src/Constant.h
@@ -2,7 +2,8 @@
 #include <SFML/Graphics.hpp>
 
 namespace constant {
-	extern sf::Font fontArial;
+    extern sf::Font fontArial;
+    extern sf::Font fontOpenSans;
 
 	void loadFont();
 } // namespace constant
